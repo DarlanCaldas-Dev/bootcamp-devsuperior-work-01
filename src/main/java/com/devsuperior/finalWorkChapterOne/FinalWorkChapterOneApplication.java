63e8d6d75ca2d69306d3505c995747ab4a18af42
Darlan Caldas <dsccaldasdev@gmail.com>
2021-02-15 00:31:07 -0300

    Initial commit

---
+++ src/main/java/com/devsuperior/finalWorkChapterOne/FinalWorkChapterOneApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.finalWorkChapterOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalWorkChapterOneApplication {
+
+ 	public static void main(String[] args) {
+		SpringApplication.run(FinalWorkChapterOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/finalWorkChapterOne/FinalWorkChapterOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.finalWorkChapterOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalWorkChapterOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
